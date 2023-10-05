e686d935a21229ad1f37651d218f7d47e647c9ac
Cainã Milech <cainamilech15@hotmail.com>
2023-10-05 10:13:36 -0300

    feat: adiciona functions de ordem

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,6 +5,30 @@ const serverUrl = "http://localhost:5000";
 function main() {
   console.log(serverUrl);
 
+  //ELEMENTOS
+  const carregarMais = document.getElementById("carregar-mais");
+  const carregarMaisPrecoMenor = document.getElementById("mais-preco-menor");
+  const carregarMaisPrecoMaior = document.getElementById("mais-preco-maior");
+  const carregarMaisRecente = document.getElementById("carregar-mais-recente");
+  const precoMaior = document.getElementById("preco-maior");
+  const precoMenor = document.getElementById("preco-menor");
+  const maisRecente = document.getElementById("mais-recente");
+
+  //EVENTOS
+  carregarMais.addEventListener("click", carregar9produtos);
+  carregarMaisPrecoMenor.addEventListener("click", carregar9produtosPrecoMenor);
+  carregarMaisPrecoMaior.addEventListener("click", carregar9produtosPrecoMaior);
+  carregarMaisRecente.addEventListener("click", carregar9produtosMaisRecente);
+  precoMenor.addEventListener("click", () => {
+    window.location.hash = "#/ordenar-preco-menor";
+  });
+  precoMaior.addEventListener("click", () => {
+    window.location.hash = "#/ordenar-preco-maior";
+  });
+  maisRecente.addEventListener("click", () => {
+    window.location.hash = "#/ordenar-mais-recente";
+  });
+
   // Consumir os primeiros 9 produtos da API
   async function fetchProducts() {
     try {
@@ -62,9 +86,136 @@ function main() {
     }
   }
 
-  //Evento carregar mais
-  const carregarMais = document.getElementById("carregar-mais");
-  carregarMais.addEventListener("click", carregar9produtos);
+  function carregar9produtosPrecoMenor() {
+    //let produtosCarregados = 0;
+
+    //const carregarProdutos = allProducts.slice(0, produtosCarregados + 9);
+    produtosCarregados += 9;
+
+    const sortedProductList = ordenarMenorPreco(allProducts);
+    console.log(sortedProductList);
+    renderProducts(sortedProductList);
+    if (produtosCarregados >= allProducts.length) {
+      carregarMaisPrecoMenor.style.display = "none"; //se a quantidade de produtos carregados for = ou > ao total de produtos, remover botao
+    }
+  }
+
+  function carregar9produtosPrecoMaior() {
+    produtosCarregados += 9;
+    const sortedProductList = ordenarMaiorPreco(allProducts);
+    console.log(sortedProductList);
+    renderProducts(sortedProductList);
+    if (produtosCarregados >= allProducts.length) {
+      carregarMaisPrecoMaior.style.display = "none"; //se a quantidade de produtos carregados for = ou > ao total de produtos, remover botao
+    }
+  }
+
+  function carregar9produtosMaisRecente() {
+    produtosCarregados += 9;
+    const sortedProductList = ordenarMaisRecente(allProducts);
+    console.log(sortedProductList);
+    renderProducts(sortedProductList);
+    if (produtosCarregados >= allProducts.length) {
+      carregarMaisRecente.style.display = "none"; //se a quantidade de produtos carregados for = ou > ao total de produtos, remover botao
+    }
+  }
+
+  function ordenarMenorPreco(products: Product[]): Product[] {
+    return products.slice().sort((a, b) => a.price - b.price);
+  }
+
+  function ordenarMaiorPreco(products: Product[]): Product[] {
+    return products.slice().sort((a, b) => b.price - a.price);
+  }
+
+  function ordenarMaisRecente(products: Product[]): Product[] {
+    return products.slice().sort((a, b) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      return dateB.getTime() - dateA.getTime();
+    });
+  }
+
+  //ASSISTIR MUDANÇA DE HASH
+  window.addEventListener("hashchange", () => {
+    if (window.location.hash == "#/ordenar-preco-maior") {
+      console.log("ordenando preco maior"); //para teste
+      //Declarar quantidade inicial
+      let produtosCarregados = 0;
+      //Criar lista de todos os produtos em ordem de preco
+      const produtosOrdenados = ordenarMaiorPreco(allProducts);
+      //Separar apenas os 9 primeiros
+      const carregarProdutos = produtosOrdenados.slice(
+        0,
+        produtosCarregados + 9
+      );
+      //Atualizar a quantidade, pra ter o controle do botão
+      produtosCarregados += 9;
+      //Atualizar a ordem dos 9
+      const listaFinal = ordenarMaiorPreco(carregarProdutos);
+      //Renderizar a lista final com o html da funcao render
+      renderProducts(listaFinal);
+      //Habilitar botão de ver mais
+      carregarMaisPrecoMaior.style.display = "block";
+      carregarMaisPrecoMenor.style.display = "none";
+      carregarMais.style.display = "none";
+      carregarMaisRecente.style.display = "none";
+    } else if (window.location.hash == "#/ordenar-preco-menor") {
+      console.log("ordenando preco menor"); //para testes
+      //Declarar quantidade inicial
+      let produtosCarregados = 0;
+      //Criar lista de todos os produtos em ordem de preco
+      const produtosOrdenados = ordenarMenorPreco(allProducts);
+      //Separar apenas os 9 primeiros
+      const carregarProdutos = produtosOrdenados.slice(
+        0,
+        produtosCarregados + 9
+      );
+      //Atualizar a quantidade, pra ter o controle do botão
+      produtosCarregados += 9;
+      //Atualizar a ordem dos 9
+      const listaFinal = ordenarMenorPreco(carregarProdutos);
+      //Renderizar a lista final com o html da funcao render
+      renderProducts(listaFinal);
+      //Habilitar botão de ver mais
+      carregarMaisPrecoMenor.style.display = "block";
+      carregarMaisPrecoMaior.style.display = "none";
+      carregarMais.style.display = "none";
+      carregarMaisRecente.style.display = "none";
+    } else if (window.location.hash == "#/ordenar-mais-recente") {
+      console.log("ordenando mais recente"); //para testes
+      //Declarar quantidade inicial
+      let produtosCarregados = 0;
+      //Criar lista de todos os produtos em ordem de preco
+      const produtosOrdenados = ordenarMaisRecente(allProducts);
+      //Separar apenas os 9 primeiros
+      const carregarProdutos = produtosOrdenados.slice(
+        0,
+        produtosCarregados + 9
+      );
+      //Atualizar a quantidade, pra ter o controle do botão
+      produtosCarregados += 9;
+      //Atualizar a ordem dos 9
+      const listaFinal = ordenarMaisRecente(carregarProdutos);
+      //Renderizar a lista final com o html da funcao render
+      renderProducts(listaFinal);
+      //Habilitar botão de ver mais
+      carregarMaisRecente.style.display = "block";
+      carregarMaisPrecoMenor.style.display = "none";
+      carregarMaisPrecoMaior.style.display = "none";
+      carregarMais.style.display = "none";
+    } else if (window.location.hash == "") {
+      carregarMaisPrecoMaior.style.display = "none";
+      carregarMaisPrecoMenor.style.display = "none";
+      carregarMaisRecente.style.display = "none";
+    }
+  });
+
+  //document.getElementById("sortButton")?.addEventListener("click", () => {
+  //  const sortedProductList = ordenarMenorPreco(allProducts);
+  //console.log(sortedProductList);
+  //renderProducts(sortedProductList);
+  //});
 
   // DESCARTADO NO MOMENTO Carregar todos produtos da API
   //async function fetchAllProducts() {
@@ -83,27 +234,27 @@ function main() {
   //}
 
   //teste para pegar o array
-  function waitForProducts() {
-    return new Promise((resolve) => {
-      // Verificar se os produtos já foram carregados
-      if (allProducts.length > 0) {
-        resolve(allProducts);
-      } else {
-        // Se não foram carregados, esperar até que sejam carregados pela função fetchProducts
-        const intervalId = setInterval(() => {
-          if (allProducts.length > 0) {
-            clearInterval(intervalId);
-            resolve(allProducts);
-          }
-        }, 100);
-      }
-    });
-  }
+  //function waitForProducts() {
+  //return new Promise((resolve) => {
+  // Verificar se os produtos já foram carregados
+  //if (allProducts.length > 0) {
+  //resolve(allProducts);
+  //} else {
+  // Se não foram carregados, esperar até que sejam carregados pela função fetchProducts
+  //const intervalId = setInterval(() => {
+  //if (allProducts.length > 0) {
+  //clearInterval(intervalId);
+  //resolve(allProducts);
+  //}
+  //}, 100);
+  //}
+  //});
+  //}
 
   // Exemplo de como acessar os produtos fora da função fetchProducts
-  waitForProducts().then((products) => {
-    console.log("Produtos carregados:", products);
-  });
+  //waitForProducts().then((products) => {
+  //console.log("Produtos carregados:", products);
+  //  });
 }
 
 document.addEventListener("DOMContentLoaded", main);
